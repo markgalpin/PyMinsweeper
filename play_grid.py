bc781ffd5ac09309d6fb19feb270495acf9f91ba
Mark Galpin <mark@tidelift.com>
2025-10-01 16:23:47 -0700

    Fully implemented tests

--- play_grid.py
+++ play_grid.py
@@ -5,16 +5,26 @@ but for now this covers the grid only as is for any implementation.
 __author__ = 'Mark Galpin'
 __version__ = '0.1'
 
-from collections import namedtuple
+from typing import NamedTuple
+import random
 
 DEFAULT_GRID_COLS=17
 DEFAULT_GRID_ROWS=24
 DEFAULT_NUM_MINES=85
 
-RevealedValue = namedtuple('RevealedValue', ['is_mined','value'])
-FullValue = namedtuple('FullValue', ['is_revealed', 'is_flagged', 'value'])
+class RevealedValue(NamedTuple):
+    """NamedTuple for what happens when you reveal a square"""
+    is_mined: bool
+    value: int
+
+class FullValue(NamedTuple):
+    """NamedTuple for the value of a square"""
+    is_revealed: bool
+    is_flagged: bool
+    value: int
 
 class Cell:
+    """A class for a single cell in the minesweeper play grid"""
     is_revealed:        bool = False
     is_flagged:         bool = False
     is_mined:           bool = False
@@ -25,26 +35,28 @@ class Cell:
         """Flags a cell.  Also has the ability to unflag by passing False"""
         assert self._is_initialized(), "The cell is not initialized"
         self.is_flagged = flag_val
-        return
 
-    def full_value(self) -> FullValue[bool, bool, int]:
-            """ Returns the value of a cell as a tuple is_flagged, is_revealed, and an int (or None if its not revealed) """
-            return (self.is_flagged, self.is_revealed, self._value)
-        
+    def full_value(self) -> FullValue:
+        """ Returns the value of a cell as a tuple is_flagged, is_revealed, 
+        and an int (or None if its not revealed) 
+        """
+        return (self.is_flagged, self.is_revealed, self._value)
+
     def revealed_value(self) -> int:
-            """ Returns the int value of a revealed cell. This function has an assertion that the cell is, in fact, revealed"""
-            result=self.full_value()
-            assert not result.is_revealed and not result.is_flagged
-            return result.value
+        """ Returns the int value of a revealed cell. 
+        This function has an assertion that the cell is, in fact, revealed"""
+        result=self.full_value()
+        assert not result.is_revealed and not result.is_flagged
+        return result.value
 
-    def reveal(self) -> RevealedValue[bool, int]:
+    def reveal(self) -> RevealedValue:
         """Reveals a single cell
             returns tuple[bool,int]: a tuple containing whether the cell is mined, and the value
             returns None if the cell is flagged, as it does nothing in that case.
         """
         assert self._is_initialized(), "The cell is not initialized"
-        if self.is_flagged: 
-            return None   #Do nothing if the cell is flagged.  I am not asserting this to deal with human players mis-clicking.
+        if self.is_flagged:
+            return None   #Do nothing if the cell is flagged.  I am not asserting this yet
         else:
             self.is_revealed = True
             return self.is_mined, self._value
@@ -52,52 +64,58 @@ class Cell:
     def __init__(self, surrounds: int = 8) -> None:
         self.surrounding_cells = surrounds #I haven't decided whether I will use this or not.
         return
-    
+
     def _is_initialized(self) -> bool:
-        if self.is_mined == False and self._value == None:
+        if not self.is_mined and self._value is None:
             return False
         else:
             return True
-    
+
     def _set_mined(self) -> None:
         self.is_mined = True
         return
-    
+
     def _set_value(self, value: int) -> None:
-        assert self._value != None, "Can only set Cell.value once"
-        assert value >= 0 and value < self.surrounding_cells, "Cell.value must be between 0 and the number of surrounding cells"
+        assert self._value is not None, "Can only set Cell.value once"
+        assert value >= 0 and value < self.surrounding_cells, "Cell.value must be between 0 and \
+            the number of surrounding cells"
         self._value = value
         return
 
     def _set_as_edge(self) -> None:
         self.surrounding_cells=5
         return
-        
+
     def _set_as_corner(self) -> None:
         self.surrounding_cells=3
         return
 
 class Grid:
+    """This is the actual class for the base grid in play
+    NOTE: I should add syntactic sugar for direct access to Grid later if it seems useful"""
     grid: list[list[Cell]] = []
     num_rows:       int = 0
     num_cols:       int = 0
+    num_mines:      int = 0
     total_cells:    int = 0
 
     def __init__(self, rows: int, columns: int, mines: int):
-         assert rows>3, "Must be at least a 3x3 grid, row doesn't work"
-         assert columns>3, "Must be at least a 3x3 grid, column doesn't work"
-         assert (mines > 0), "Must have at least 1 mine"
-         self.num_rows=rows
-         self.num_cols=columns
-         self.total_cells=rows * columns
-         assert (mines<self.total_cells), "mines must be less than the total number of cells"
-         self._create_cells()
-         self._populate_mines(mines)
-         self._calculate_cell_values()
-         return
+        assert rows>3, "Must be at least a 3x3 grid, row doesn't work"
+        assert columns>3, "Must be at least a 3x3 grid, column doesn't work"
+        assert (mines > 0), "Must have at least 1 mine"
+        self.num_rows=rows
+        self.num_cols=columns
+        self.total_cells=rows * columns
+        assert (mines<self.total_cells), "mines must be less than the total number of cells"
+        self._create_cells()
+        self._populate_mines(mines)
+        self._calculate_cell_values()
+        return
 
     def _create_cells(self) -> None:
-        self.grid = [[Cell()] * self.num_cols] * self.num_rows
+        #pylint: disable=protected-access
+        #allowing Grid protected access to Cell is fine.
+        self.grid = [[Cell() for _ in range(self.num_cols)] for _ in range(self.num_rows)]
         for cell in self.grid[0]: # Top Edge
             cell._set_as_edge()
         for cell in self.grid[self.num_rows-1]: #bottom edge
@@ -110,19 +128,21 @@ class Grid:
         self.grid[0][self.num_cols-1]._set_as_corner()
         self.grid[self.num_rows-1][0]._set_as_corner()
         self.grid[self.num_rows-1][self.num_cols-1]._set_as_corner()
+        #pylint: enable=protected-access
         return
-    
+
     def _populate_mines(self, mines: int):
-         #There should probably be a rule to prevent mines from fully surrounding a square (e.g. generating an 8)
-         # or even a full-up block of 3x3 square mines.  But for now, I am going to not worry about it.
-         # NOTE: It could be interesting to see how it affects the win rate to implement a block limit rule.  Something to think about later.
-         raise NotImplementedError()
-    
+        #There should probably be a rule to prevent mines from fully surrounding a square
+        # (e.g. generating an 8) or even a full-up block of 3x3 square mines.
+        # But for now, I am going to not worry about it.
+        # NOTE: It could be how a block limit rule it affects the win rate.
+        while self.num_mines < mines:
+            row_i=random.randint(0, self.num_rows-1)
+            col_i=random.randint(0, self.num_cols-1)
+            if not self.grid[row_i][col_i].is_mined:
+                self.grid[row_i][col_i]._set_mined() #pylint: disable=protected-access
+                self.num_mines+=1
+        return NotImplementedError()
+
     def _calculate_cell_values(self):
-         raise NotImplemented
-
-def test(rows: int, columns: int, mines:int) -> Grid:
-    MyGrid = Grid(rows, columns, mines)
-    return
-
-test(DEFAULT_GRID_ROWS, DEFAULT_GRID_COLS, DEFAULT_NUM_MINES)
+        return NotImplementedError()
---
+++ test_play_grid.py
@@ -0,0 +1,80 @@
+""" 
+This is the tests for play_grid
+"""
+
+__author__ = 'Mark Galpin'
+__version__ = '0.1'
+import pytest
+import play_grid
+from play_grid import Grid
+
+
+
+num_cols = play_grid.DEFAULT_GRID_COLS
+
+num_rows = play_grid.DEFAULT_GRID_ROWS
+
+num_mines = play_grid.DEFAULT_NUM_MINES
+
+mygrid = Grid(num_rows, num_cols, num_mines)
+
+def test_cell_separation():
+    assert mygrid.grid[0][0] is not mygrid.grid[0][1]
+    assert mygrid.grid[0][0] is not mygrid.grid[1][0]
+
+def test_size():
+    assert mygrid.num_cols == num_cols
+    assert mygrid.num_rows == num_rows
+    assert mygrid.total_cells == (num_rows * num_cols)
+
+def test_surrounds():
+    """Test surrounds.  Should be 3 in the corners, 5 on the edges, and otherwise 8
+    """
+    for row_index, row in enumerate(mygrid.grid):
+        assert row_index < num_rows
+        for column_index, cell in enumerate(row):
+            assert column_index < num_cols
+            if row_index == 0 or row_index == (num_rows - 1):
+                if column_index == 0 or column_index == (num_cols - 1): # test the corners and top/bottom edges
+                    assert cell.surrounding_cells == 3, "Row: "+row_index.__str__()+" Column: "+column_index.__str__()
+                else:
+                    assert cell.surrounding_cells == 5, "Row: "+row_index.__str__()+" Column: "+column_index.__str__()
+            else:
+                if column_index == 0 or column_index == (num_cols - 1): # test the left/right edges:
+                    assert cell.surrounding_cells == 5, "Row: "+row_index.__str__()+" Column: "+column_index.__str__()
+                else: #test everything else
+                    assert cell.surrounding_cells == 8, "Row: "+row_index.__str__()+" Column: "+column_index.__str__()
+    return
+
+@pytest.mark.xfail
+def test_mines():
+    """Tests that the number of mines is correct"""
+    count = 0
+    for row in mygrid.grid:
+        for cell in row:
+            if cell.is_mined: count+=1
+    assert count == num_mines
+
+def test_unrevealed():
+    """Tests that all squares in the initial set are unrevealed"""
+    count = 0
+    for row in mygrid.grid:
+        for cell in row:
+            if cell.is_revealed: count+=1
+    assert count == 0
+
+def test_unflagged():
+    """Tests that all squares in the initial set are unflagged"""
+    count = 0
+    for row in mygrid.grid:
+        for cell in row:
+            if cell.is_flagged: count+=1
+    assert count == 0
+
+@pytest.mark.xfail
+def test_values_set():
+    """Tests that all squares have a configured value"""
+    for row in mygrid.grid:
+        for cell in row:
+            if not cell.is_mined:
+                assert cell._value is not None #pylint: disable=protected-access
